afd73d247c8f96c99d5204240cdb74b2f3f7de79
RENAN DO NASCIMENTO LISBOA <renan.lisboa@academico.ifrn.edu.br>
2023-07-24 15:57:59 -0300

    implementa classe NCliente.py

--- app/NCliente.py
+++ app/NCliente.py
@@ -4,58 +4,49 @@ from cliente import Cliente
 from NUsuario import NUsuario
 class NCliente:
     def __init__(self):
-        self.clientes = self.ler_arquivo('cliente.json')
+        self.clientes = self.ler_arquivo('clientes.json')
     
     def listar(self):
         return self.clientes
     
     def ver(self, id):
-        for cliente in self.clientes:
-            if cliente.get_id() == id:
+        clientes = list(self.listar().values())
+        for cliente in clientes:
+            if cliente['id'] == int(id):
                 return cliente
         return None
     
     def cadastrar(self, nome, email, senha, cpf):
-        clientes = self.listar()
-        list_clientes = list(clientes.values())
-        novos_clientes = []
+        resposta_usuario = NUsuario().cadastrar(nome, email, senha)
         
-        usuario = NUsuario().cadastrar(nome, email, senha)
+        
+        usuario_id = resposta_usuario['usuario_id'] if 'usuario_id' in resposta_usuario else None
+        if not usuario_id: return resposta_usuario
+        
+        clientes = self.listar()
+        lista_clientes = list(clientes.values())
+      
+        cliente_id = len(lista_clientes) + 1
+        novo_cliente = Cliente(cliente_id, usuario_id, cpf)
+        nova_lista_clientes = []
 
-        id_cliente = len(list_clientes) + 1
-        novo_cliente = Cliente(id_cliente, usuario["data"]["usuario"].get_id(), cpf)
-        for cliente in list_clientes:
-            c = Cliente(cliente['id'], cliente['usuario_id'], cliente['cpf'])
-            novos_clientes.append(c)
-        novos_clientes.append(novo_cliente)
-
-        self.grava_arquivo(novos_clientes, 'clientes.json')
-        return {"data":{"message": "Cliente cadastrado com sucesso!", "cliente": novo_cliente}, "status": "201"}
+        for cliente in lista_clientes:
+            cliente = Cliente(cliente['id'], cliente['usuario'], cliente['cpf'])
+            nova_lista_clientes.append(cliente)
+  
+        nova_lista_clientes.append(novo_cliente)
+        
+        self.grava_arquivo(nova_lista_clientes, "clientes.json")
+        return "Cadastro realizado"
+        
     
     def atualizar(self, id, nome, email, senha, cpf):
-        cliente = self.ver(id)
-        if cliente:
-            if nome:
-                cliente.set_nome(nome)
-            if email:
-                cliente.set_email(email)
-            if senha:
-                cliente.set_senha(senha)
-            if cpf:
-                cliente.set_cpf(cpf)
-            self.grava_arquivo(self.clientes, 'cliente.json')
-            return {"message": "Cliente atualizado com sucesso!", "status": "200"}
-        else:
-            return {"message": "Cliente não encontrado.", "status": "400"}
+        resposta_usuario = NUsuario().cadastrar(id, nome, email, senha)
+        return resposta_usuario
     
     def excluir(self, id):
-        cliente = self.ver(id)
-        if cliente:
-            self.clientes.remove(cliente)
-            self.grava_arquivo(self.clientes, 'cliente.json')
-            return {"message": "Cliente removido com sucesso!", "status": "200"}
-        else:
-            return {"message": "Cliente não encontrado.", "status": "400"}
+        resposta_usuario = NUsuario().cadastrar(id)
+        return resposta_usuario
     
     @staticmethod
     def grava_arquivo(clientes: list, arquivo: str):
@@ -88,47 +79,48 @@ class NCliente:
             return clientes
         except:
             return {}
-n_cliente = NCliente()
-while True:
-    print("1 - Listar clientes")
-    print("2 - Ver um cliente")
-    print("3 - Cadastrar cliente")
-    print("4 - Remover cliente")
-    print("5 - Atualizar cliente")
-    print("6 - Sair")
-    opt = input("O que deseja fazer? ")
+n_cliente = NCliente().ver(2)
+print(n_cliente)
+# while True:
+#     print("1 - Listar clientes")
+#     print("2 - Ver um cliente")
+#     print("3 - Cadastrar cliente")
+#     print("4 - Remover cliente")
+#     print("5 - Atualizar cliente")
+#     print("6 - Sair")
+#     opt = input("O que deseja fazer? ")
     
-    if opt == '1':
-        clientes = n_cliente.listar()
-        for cliente in clientes:
-            print(cliente)
-    elif opt == '2':
-        id = int(input("ID do cliente: "))
-        cliente = n_cliente.ver(id)
-        if cliente:
-            print(cliente)
-        else:
-            print("Cliente não encontrado.")
-    elif opt == '3':
-        nome = input("Nome: ")
-        email = input("Email: ")
-        senha = input("Senha: ")
-        cpf = input("CPF: ")
-        resposta = n_cliente.cadastrar(nome, email, senha, cpf)
-        print(resposta["data"])
-    elif opt == '4':
-        id = int(input("ID do cliente: "))
-        resposta = n_cliente.excluir(id)
-        print(resposta["data"])
-    elif opt == '5':
-        id = int(input("ID do cliente: "))
-        nome = input("Nome: ")
-        email = input("Email: ")
-        senha = input("Senha: ")
-        cpf = input("CPF: ")
-        resposta = n_cliente.atualizar(id, nome, email, senha, cpf)
-        print(resposta["data"])
-    elif opt == '6':
-        break
-    else:
-        print("Opção inválida. Tente novamente.")
+#     if opt == '1':
+#         clientes = n_cliente.listar()
+#         for cliente in clientes:
+#             print(cliente)
+#     elif opt == '2':
+#         id = int(input("ID do cliente: "))
+#         cliente = n_cliente.ver(id)
+#         if cliente:
+#             print(cliente)
+#         else:
+#             print("Cliente não encontrado.")
+#     elif opt == '3':
+#         nome = input("Nome: ")
+#         email = input("Email: ")
+#         senha = input("Senha: ")
+#         cpf = input("CPF: ")
+#         resposta = n_cliente.cadastrar(nome, email, senha, cpf)
+#         print(resposta)
+#     elif opt == '4':
+#         id = int(input("ID do cliente: "))
+#         resposta = n_cliente.excluir(id)
+#         print(resposta)
+#     elif opt == '5':
+#         id = int(input("ID do cliente: "))
+#         nome = input("Nome: ")
+#         email = input("Email: ")
+#         senha = input("Senha: ")
+#         cpf = input("CPF: ")
+#         resposta = n_cliente.atualizar(id, nome, email, senha, cpf)
+#         print(resposta)
+#     elif opt == '6':
+#         break
+#     else:
+#         print("Opção inválida. Tente novamente.")
--- app/NUsuario.py
+++ app/NUsuario.py
@@ -24,12 +24,12 @@ class NUsuario:
         usuarios = self.ler_arquivo('usuarios.json')
         list_usuarios = list(usuarios.values())
 
-        if '@' not in email: return {"message":"Formato de email inválido.", "status": "400"}
+        if '@' not in email: return "Formato de email inválido."
         email_ja_existe = [user for user in list_usuarios if user['email'] == email]
         
-        if len(email_ja_existe) > 0: return {"message":"Email já cadastrado.", "status": "400"}
+        if len(email_ja_existe) > 0: return "Email já cadastrado."
         
-        if len(nome) < 3  or len(senha) < 8: return {"message":"Mínimo de 3 caracteres para nome e 8 para senha", "status": "400"}
+        if len(nome) < 3  or len(senha) < 8: return "Mínimo de 3 caracteres para nome e 8 para senha"
         
         id =  len(list_usuarios) + 1
         usuario = Usuario(id, nome, email, senha)
@@ -41,7 +41,7 @@ class NUsuario:
         novos_usuarios.append(usuario)
 
         self.grava_arquivo(novos_usuarios, 'usuarios.json')
-        return {"data":{"message": "Usuário cadastrado com sucesso!", "usuario": usuario}, "status": "201"}
+        return {"message": "Usuário cadastrado com sucesso!", "usuario_id": usuario.get_id()}
     
     def atualizar(self, id, nome, email, senha):
         usuario = self.ver_usuario(id)['data']
@@ -49,21 +49,21 @@ class NUsuario:
         list_usuarios = list(usuarios.values())
        
         if senha != '':
-            if len(senha) < 8: return {"message":"Mínimo de 8 para senha", "status": "400"}
+            if len(senha) < 8: return "Mínimo de 8 para senha"
         if senha == '':
             senha = usuario['senha'] 
 
         if email != '':
-            if '@' not in email: return {"message":"Formato de email inválido.", "status": "400"}
+            if '@' not in email: return "Formato de email inválido."
             email_ja_existe = [user for user in list_usuarios if user['email'] == email]
             if len(email_ja_existe) > 0: 
-                return {"message":"Email já cadastrado.", "status": "400"}
+                return "Email já cadastrado."
         if email == '':
             email = usuario['email']
         
         if nome != '':
             if len(nome) < 3:
-                return {"message":"Mínimo de 3 caracteres para nome", "status": "400"}
+                return "Mínimo de 3 caracteres para nome"
         if nome == '':
             nome = usuario['nome']
         
@@ -80,7 +80,7 @@ class NUsuario:
             novos_usuarios.append(u)
         novos_usuarios.append(usuario_atualizado)
         self.grava_arquivo(novos_usuarios, 'usuarios.json')
-        return {"message": "Usuário atualizado com sucesso!", "status": "200"}
+        return {"message": "Usuário atualizado com sucesso!", "usuario_id": usuario_atualizado.get_id}
 
     def excluir(self, id):
         usuarios = self.listar()
